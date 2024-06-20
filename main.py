fdc3ee858c376363a51cdf822710fcb09ceb73d6
StNiSlv <159066869+StNiSlv@users.noreply.github.com>
2024-06-20 16:24:18 +0300

    Add files via upload
    Первые 5 вводных задач

---
+++ main.py
@@ -0,0 +1,5 @@
+first = 1
+second = 2
+summa = first + second
+diff = first - second
+print(summa, diff)
---
+++ main2.py
@@ -0,0 +1,5 @@
+first = 1
+second = 2
+third = 3
+mean = (first + second + third)/3
+print(mean)
