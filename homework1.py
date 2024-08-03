5dad8e2aaac6126032d337fd0bd1104540026e18
Stanislav <159066869+StNiSlv@users.noreply.github.com>
2024-08-04 02:48:38 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,6 @@
+example = 'Аргентина'
+print(example[0])
+print(example[8])
+print(example[5::1])
+print(example[::-1])
+print(example[1::2])
---
+++ Переменные.py
@@ -0,0 +1,5 @@
+Complete_homework = 12
+hours_spent = 1.5
+Course = "Python"
+Time_for_one_task = hours_spent /Complete_homework
+print("Курс:", Course, ", всего задач:", Complete_homework, ", затрачено часов:", hours_spent, ", среднее время выполнения", Time_for_one_task, "часа")
