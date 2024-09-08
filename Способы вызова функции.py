98a7a03a7278f09c16a2825d31593f09975d93ad
Stanislav <159066869+StNiSlv@users.noreply.github.com>
2024-09-08 07:38:33 +0300

    Способы вызова функции.py
    Исправлено

--- Способы вызова функции.py
+++ Способы вызова функции.py
@@ -1,13 +1,15 @@
 def send_email(message, recipient, sender="university.help@gmail.com"):
-    if recipient.count("@") == 1 and sender.count("@") == 1 and recipient.count(".com") == 1 or recipient.count(".net") == 1 or recipient.count(".ru") == 1 and sender.count(".com") == 1 or sender.count(".net") == 1 or sender.count(".ru") == 1:
-        if sender == recipient:
-            print("Нельзя отправить письмо самому себе!")
-        elif sender == "university.help@gmail.com":
-            print("Письмо успешно отправлено с адреса ", sender, " на адрес", recipient)
-        elif sender != "university.help@gmail.com":
-            print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса ", sender, "на адрес ", recipient)
+    def is_valid_email(email):
+        return "@" in email and (email.endswith(".com") or email.endswith(".ru") or email.endswith(".net"))
+    if not is_valid_email(sender) or not is_valid_email(recipient):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    elif sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+    elif sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
     else:
-        print("Невозможно отправить письмо с адреса ", sender, "на адрес ", recipient)
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
