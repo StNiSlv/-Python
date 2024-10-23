dbe619eeb4a21cb1424c3b1a797fcccc560bb6a8
Stanislav <159066869+StNiSlv@users.noreply.github.com>
2024-10-24 01:35:21 +0300

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,7 @@
+def add_everything_up(a, b):
+    if type(a) != type(b):
+        return str(a) + str(b)
+    return a + b
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
