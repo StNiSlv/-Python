0b3db6913d3e3d3d0734e88a81700f82b2ecdde5
Stanislav <159066869+StNiSlv@users.noreply.github.com>
2024-11-18 00:51:37 +0300

    Add files via upload

---
+++ main_1.py
@@ -0,0 +1,10 @@
+#1st program#
+print((9**0.5)*5)
+#2st program#
+print(9.98<9.99 and 1000 != 1000.1)
+#3st program#
+print(2*2+2 == 2*(2+2))
+#4st program#
+stroka = "123.456"
+stroka1 = int(float(stroka)*10)
+print(str(stroka1)[-1])
