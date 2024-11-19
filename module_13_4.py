df1e028d4a4ff38ee4f229a84e3c70d04488eb7b
Stanislav <159066869+StNiSlv@users.noreply.github.com>
2024-11-19 05:25:59 +0300

    Add files via upload

---
+++ module_13_4.py
@@ -0,0 +1,54 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+
+api = "7757596943:AAFcIONrNKyvbErjJ7fBNvPrL40DVOd1exk"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+@dp.message_handler(lambda message: message.text.lower() == 'calories')
+async def set_age(message: types.Message):
+    await UserState.age.set()
+    await message.answer("Введите свой возраст:")
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    await state.update_data(age=int(message.text))
+    await UserState.growth.set()
+    await message.answer("Введите свой рост (в сантиметрах):")
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state: FSMContext):
+    await state.update_data(growth=int(message.text))
+    await UserState.weight.set()
+    await message.answer("Введите свой вес (в килограммах):")
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message: types.Message, state: FSMContext):
+    await state.update_data(weight=int(message.text))
+
+    data = await state.get_data()
+    age = data['age']
+    growth = data['growth']
+    weight = data['weight']
+
+    calories = 88.36 + (13.4 * weight) + (4.8 * growth) - (5.7 * age)
+
+    await message.answer(f"Ваша норма калорий: {calories:.2f} ккал.")
+
+    await state.finish()
+
+
+@dp.message_handler()
+async def all_messages(message: types.Message):
+    await message.answer("Введите 'Calories', чтобы рассчитать норму калорий.")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
