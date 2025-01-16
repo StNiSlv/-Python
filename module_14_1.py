846d7bf62aafdc1d727b654a460eaf609847d671
Stanislav <159066869+StNiSlv@users.noreply.github.com>
2025-01-17 02:31:50 +0300

    Add files via upload

--- module_14_1.py
+++ module_14_1.py
@@ -1,7 +1,7 @@
 import sqlite3
 
-connection = sqlite3.connect('not_telegram.db')
-cursor = connection.cursor()
+conn = sqlite3.connect('not_telegram.db')
+cursor = conn.cursor()
 
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Users (
@@ -13,35 +13,20 @@ CREATE TABLE IF NOT EXISTS Users (
 )
 ''')
 
-users_data = [
-    (f'User{i}', f'example{i}@gmail.com', i * 10, 1000)
-    for i in range(1, 11)
-]
-cursor.executemany('''
-INSERT INTO Users (username, email, age, balance) 
-VALUES (?, ?, ?, ?)
-''', users_data)
+users = [(i, f"User{i}", f"example{i}@gmail.com", i * 10, 1000) for i in range(1, 11)]
+cursor.executemany('INSERT OR IGNORE INTO Users VALUES (?, ?, ?, ?, ?)', users)
 
-cursor.execute('''
-UPDATE Users
-SET balance = 500
-WHERE id % 2 = 1
-''')
+cursor.execute('UPDATE Users SET balance = 500 WHERE id % 2 != 0')
 
-cursor.execute('''
-DELETE FROM Users
-WHERE id % 3 = 0
-''')
+cursor.execute('SELECT id FROM Users')
+id_to_delete = [row[0] for row in cursor.fetchall()][::3]
+cursor.execute(f'DELETE FROM Users WHERE id IN ({",".join(map(str, id_to_delete))})')
 
-cursor.execute('''
-SELECT username, email, age, balance
-FROM Users
-WHERE age != 60
-''')
-result = cursor.fetchall()
-
-for row in result:
+cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != 60')
+for row in cursor.fetchall():
     print(f"Имя: {row[0]} | Почта: {row[1]} | Возраст: {row[2]} | Баланс: {row[3]}")
 
-connection.commit()
-connection.close()
+conn.commit()
+conn.close()
+
+
